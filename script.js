a2f191de6aa97e48bf224cc5939f9a44b77992b0
aji70 <113303791+aji70@users.noreply.github.com>
2024-01-01 21:58:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+//https://countries-api-836d.onrender.com/countries/
+///////////////////////////////////////
+const request = new XMLHttpRequest();
+
+request.open('GET', 'https://countries-api-836d.onrender.com/countries')
+request.send();
+
+request.addEventListener('load', function(){
+    console.log(this.responseText);
+
+})

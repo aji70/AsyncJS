4b73c14fd466fd57af9e3adab608083b0a2b9b58
aji70 <113303791+aji70@users.noreply.github.com>
2024-01-03 15:13:04 +0100

    Async

--- script.js
+++ script.js
@@ -3,11 +3,12 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 //https://countries-api-836d.onrender.com/countries/
-///////////////////////////////////////
+////////////////////////////////////////
 const request = new XMLHttpRequest();
 
 request.open('GET', 'https://countries-api-836d.onrender.com/countries')
 request.send();
+
 
 request.addEventListener('load', function(){
     console.log(this.responseText);
